04d620543959b620c41e8326947e2e88842812bc
yyyangyh <106877195+yyyangyh@users.noreply.github.com>
2022-12-28 20:01:19 +0800

    Update panback.py

--- dem/code/panback.py
+++ dem/code/panback.py
@@ -5,7 +5,7 @@ country_id = 3  # 以最小的country_id為基準
 delta_dem = []
 base = 0  # 該國2018的score
 for i in range (716):
-    # 同國家時country_id同，比較年分，若為2018(基礎年)，則將該年score設為減數，在list中添加na；否則添加(該年 score - 2018 score)四捨五入到小術後第二位
+    # 同國家同country_id，比較年分，若為2018(基礎年)，則將該年score設為減數，在list中添加na；否則添加(該年score-2018score)四捨五入
     if dem_dict[i]['country_id'] == country_id:
         if dem_dict[i]['year'] == 2018:
             base = dem_dict[i]['score']
